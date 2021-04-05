3983e2902d1aaaae8fc71854f28a28d4b7046365
Tim <4759569+TimGuenther@users.noreply.github.com>
2021-04-05 17:28:24 +0200

    improve learn mode and search

--- app/app.js
+++ app/app.js
@@ -1,16 +1,25 @@
+// Delete in production
+localStorage.removeItem('dict')
+localStorage.removeItem('meta')
+localStorage.removeItem('settings')
+localStorage.removeItem('user_data')
+
 // Init localStorage to save configurations and settings
 var dict = localStorage.getItem('dict');
 var meta = localStorage.getItem('meta');
 var settings = localStorage.getItem('settings');
 var user_data = localStorage.getItem('user_data');
 
-if(dict === null){
+if (dict === null) {
     xhr1 = new XMLHttpRequest();
-    xhr1.onreadystatechange = function() {
+    xhr1.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-           // Typical action to be performed when the document is ready:
-           dict = JSON.parse(xhr1.responseText);
-           localStorage.setItem('dict', xhr1.responseText);
+            // Typical action to be performed when the document is ready:
+            dict = JSON.parse(xhr1.responseText);
+            localStorage.setItem('dict', xhr1.responseText);
+            if (document.getElementById("sign_count") !== null) {
+                document.getElementById('sign_count').innerText = dict.dict.length;
+            }
         }
     };
     xhr1.open('GET', '/app/data/dict.json', true);
@@ -19,13 +28,13 @@ if(dict === null){
     dict = JSON.parse(dict);
 }
 
-if(meta === null){
+if (meta === null) {
     xhr2 = new XMLHttpRequest();
-    xhr2.onreadystatechange = function() {
+    xhr2.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-           // Typical action to be performed when the document is ready:
-           meta = JSON.parse(xhr2.responseText);
-           localStorage.setItem('meta', xhr2.responseText);
+            // Typical action to be performed when the document is ready:
+            meta = JSON.parse(xhr2.responseText);
+            localStorage.setItem('meta', xhr2.responseText);
         }
     };
     xhr2.open('GET', '/app/data/meta.json', true);
@@ -34,13 +43,13 @@ if(meta === null){
     meta = JSON.parse(meta);
 }
 
-if(settings === null){
+if (settings === null) {
     xhr3 = new XMLHttpRequest();
-    xhr3.onreadystatechange = function() {
+    xhr3.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-           // Typical action to be performed when the document is ready:
-           settings = JSON.parse(xhr3.responseText);
-           localStorage.setItem('settings', xhr3.responseText);
+            // Typical action to be performed when the document is ready:
+            settings = JSON.parse(xhr3.responseText);
+            localStorage.setItem('settings', xhr3.responseText);
         }
     };
     xhr3.open('GET', '/app/data/settings.json', true);
@@ -49,15 +58,15 @@ if(settings === null){
     settings = JSON.parse(settings);
 }
 
-if(user_data === null){
+if (user_data === null) {
     xhr4 = new XMLHttpRequest();
-    xhr4.onreadystatechange = function() {
+    xhr4.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-           // Typical action to be performed when the document is ready:
-           user_data = JSON.parse(xhr4.responseText);
-           localStorage.setItem('user_data', xhr4.responseText);
+            // Typical action to be performed when the document is ready:
+            user_data = JSON.parse(xhr4.responseText);
+            localStorage.setItem('user_data', xhr4.responseText);
 
-           init()
+            init()
         }
     };
     xhr4.open('GET', '/app/data/user_data.json', true);
@@ -66,134 +75,161 @@ if(user_data === null){
     user_data = JSON.parse(user_data);
 }
 
-if(document.getElementById("sign_count") !== null){
+if (document.getElementById("sign_count") !== null && dict !== null) {
     document.getElementById('sign_count').innerText = dict.dict.length;
 }
 
-if(document.getElementById('search') !== null){
-    document.getElementById('search').addEventListener("click", function(data){
+if (document.getElementById('search') !== null) {
+    document.getElementById('search').addEventListener("click", function (data) {
         word = document.getElementById("search_input").value
         showSearchResult("search_result", word)
     });
 }
 
-if(document.getElementById("btn-fach-1") !== null){
-    document.getElementById("btn-fach-1").addEventListener("click", function(){
+if (document.getElementById("btn-fach-1") !== null) {
+    document.getElementById("btn-fach-1").addEventListener("click", function () {
         vs = getPracticeSet(0);
-        showPracticeHTML(vs,0)
+        showPracticeHTML(vs, 0)
     });
 }
 
 
 // Word search
 
-function searchInDict(word){
-    console.log("Search Word:",word)
+function searchInDict(word, isExactMatch) {
+    console.log("Search Word:", word)
     result = [];
 
-    for(var i = 0; i<dict.dict.length; i++){
-        if(dict.dict[i].word.de.toLowerCase() === word.toLowerCase()){
+    for (var i = 0; i < dict.dict.length; i++) {
+        if (dict.dict[i].word.de.toLowerCase() === word.toLowerCase()) {
             result.push(dict.dict[i])
             break;
         }
     }
-    for(var i = 0; i<dict.dict.length; i++){
-        if(dict.dict[i].word.de.toLowerCase().match(".*?"+word.toLowerCase()+".*?") !== null){
-            result.push(dict.dict[i])
+    if (!isExactMatch) {
+        for (var i = 0; i < dict.dict.length; i++) {
+            if (dict.dict[i].word.de.toLowerCase() === word.toLowerCase()) {
+                continue;
+            }
+            if (dict.dict[i].word.de.toLowerCase().match(".*?" + word.toLowerCase() + ".*?") !== null) {
+                result.push(dict.dict[i])
+            }
         }
     }
     return result;
 }
 
-function searchResultHTML(word, obj){
-    console.log("Obj:",obj)
-    html = "<p>Suche nach \""+encodeHTMLEntities(word)+"\".</p><hr>"
+function searchResultHTML(word, obj) {
+    console.log("Obj:", obj)
+    html = "<p>Suche nach \"" + encodeHTMLEntities(word) + "\".</p><hr>"
     tmp = html;
-    
-    if(obj.length > 0){
+
+    if (obj.length > 0) {
         html += `
+            <h4>Bestes Ergebnis:</h4>
             <br>
             <div class="card text-center">
             <div class="card-header">
-                <h4>`+encodeHTMLEntities(obj[0].word.de)+`</h4>
+                <h4>` + encodeHTMLEntities(obj[0].word.de) + `</h4>
             </div>
+            <br>
             <div class="card-img-top embed-responsive embed-responsive-4by3">
                 <video class="embed-responsive-item" autoplay muted loop>
-                    <source src="`+encodeURI(obj[0].video.dgs[0].url)+`" type="video/mp4">
+                    <source src="` + encodeURI(obj[0].video.dgs[0].url) + `" type="video/mp4">
                     Your browser does not support the video tag.
                 </video> 
+            </div>
+            <div>
+                <i class="far fa-copyright text-right" data-toggle="collapse" data-target="#info-0" aria-expanded="false" aria-controls="info-0"></i>
+                <div class="collapse text-left" id="info-0">
+                    <b>Quelle:</b> ` + obj[0].video.dgs[0].source + `<br>
+                    <b>Datum:</b> ` + obj[0].video.dgs[0].created + `<br>
+                    <b>Lizenz:</b> <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/de/">CC BY-NC-SA 3.0 DE</a>
+                </div>
             </div>
             </div>
             <br>
             <hr>
-            <h4>Worte die den Suchbergiff enthalten:</h4>
-            <br>`;
-        for(var i = 1; i<obj.length; i++){
-            if(i==10){
-                html += "<p> 10 von "+obj.length+" Ergebnissen angezeigt, bitte genauer suchen."
+            `;
+        for (var i = 1; i < obj.length; i++) {
+            if (i == 1) {
+                html += "<h4>Worte die den Suchbergiff enthalten:</h4><br>"
+            }
+            if (i == 5) {
+                html += "<p> 5 von " + obj.length + " Ergebnissen angezeigt, bitte genauer suchen."
                 break;
             }
             html += `<div class="card text-center">
-            <div class="card-header" data-toggle="collapse" data-target="#video-`+i+`" aria-expanded="false" aria-controls="video-`+i+`">
-                <h4>`+encodeHTMLEntities(obj[i].word.de)+`</h4>
+            <div class="card-header" data-toggle="collapse" data-target="#video-` + i + `" aria-expanded="false" aria-controls="video-` + i + `">
+                <h4>` + encodeHTMLEntities(obj[i].word.de) + `</h4>
             </div>
-            <div class="card-img-top embed-responsive embed-responsive-4by3 collapse" id="video-`+i+`">
+            <div class="collapse" id="video-` + i + `">
+            <div class="card-img-top embed-responsive embed-responsive-4by3">
                 <video class="embed-responsive-item" autoplay muted loop>
-                    <source src="`+encodeURI(obj[i].video.dgs[0].url)+`" type="video/mp4">
+                    <source src="` + encodeURI(obj[i].video.dgs[0].url) + `" type="video/mp4">
                     Your browser does not support the video tag.
-                </video> 
+                </video>
+            </div> 
+            <div>
+                <i class="far fa-copyright text-right" data-toggle="collapse" data-target="#info-` + i + `" aria-expanded="false" aria-controls="info-` + i + `"></i>
+                <div class="collapse text-left" id="info-` + i + `">
+                    <b>Quelle:</b> ` + obj[i].video.dgs[0].source + `<br>
+                    <b>Datum:</b> ` + obj[i].video.dgs[0].created + `<br>
+                    <b>Lizenz:</b> <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/de/">CC BY-NC-SA 3.0 DE</a>
+                </div>
+            </div>
             </div>
             </div>
             <br>`;
         }
     }
-    if(html === tmp){
+    if (html === tmp) {
         html += "<p>Kein Ergebnis gefunden.</p>"
     }
     return html;
 }
 
-function showSearchResult(id,word){
+function showSearchResult(id, word) {
     element = document.getElementById(id);
-    obj = searchInDict(word)
-    html = searchResultHTML(word,obj)
+    obj = searchInDict(word, false)
+    html = searchResultHTML(word, obj)
     element.innerHTML = html
 }
 
 // Learn new vocabulary
 
-function addToData(obj, num){
-    if(num >= 0 && num < 6){
-        user_data.data[num].push(obj)
+function addToData(obj, num) {
+    if (num >= 0 && num < 6) {
+        user_data.data[num].push(obj[0])
     }
 }
 
-function init(){
-    if(user_data.data[0].length === 0){
-        addToData(searchInDict('"Vogel"'),0);
-        addToData(searchInDict('"Baum"'),0);
-        addToData(searchInDict('"Hund"'),0);
-        addToData(searchInDict('"Katze"'),0);
-        addToData(searchInDict('"Lampe"'),0);
-        addToData(searchInDict('"Papa"'),0);
-        addToData(searchInDict('"Mama"'),0);
-        addToData(searchInDict('"Oma"'),0);
-        addToData(searchInDict('"Opa"'),0);
-        addToData(searchInDict('"Ich"'),0);
-        addToData(searchInDict('"Du"'),0);
+function init() {
+    if (user_data.data[0].length === 0) {
+        addToData(searchInDict('Vogel', true), 0);
+        addToData(searchInDict('Baum', true), 0);
+        addToData(searchInDict('Hund', true), 0);
+        addToData(searchInDict('Katze', true), 0);
+        addToData(searchInDict('Lampe', true), 0);
+        addToData(searchInDict('Papa', true), 0);
+        addToData(searchInDict('Mama', true), 0);
+        addToData(searchInDict('Oma', true), 0);
+        addToData(searchInDict('Opa', true), 0);
+        addToData(searchInDict('Ich', true), 0);
+        addToData(searchInDict('Du', true), 0);
 
         localStorage.setItem("user_data", JSON.stringify(user_data))
     }
 }
 
-function getPracticeSet(num){
+function getPracticeSet(num) {
     count = settings.number_of_words_per_session
     vocabulary_set = []
 
-    for(var i = 0; i < count; i++){
-        if(user_data.data[num] !== undefined){
+    for (var i = 0; i < count; i++) {
+        if (user_data.data[num] !== undefined) {
             v = user_data.data[num].pop();
-            console.log(i,v)
+            console.log(i, v)
             vocabulary_set.push(v)
         }
     }
@@ -201,14 +237,14 @@ function getPracticeSet(num){
     return vocabulary_set;
 }
 
-function createPracticeHTML(vocabulary_set, i){
+function createPracticeHTML(vocabulary_set, i) {
     html = `<div class="card text-center">
         <div class="card-header">
             <h4>ÜBEN</h4>
         </div>
         <div class="card-img-top embed-responsive embed-responsive-4by3">
             <video class="embed-responsive-item" autoplay muted loop>
-                <source src="`+encodeURI(vocabulary_set[i].video.dgs[0].url)+`" type="video/mp4">
+                <source src="` + encodeURI(vocabulary_set[i].video.dgs[0].url) + `" type="video/mp4">
                 Your browser does not support the video tag.
             </video>
             
@@ -221,7 +257,7 @@ function createPracticeHTML(vocabulary_set, i){
         <button class="btn btn-info"  data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Lösung anzeigen</button>
         <div class="collapse" id="collapseExample">
             <br>
-            <h5 class="card-title" id="word">`+encodeHTMLEntities(vocabulary_set[i].word.de)+`</h5>
+            <h5 class="card-title" id="word">` + encodeHTMLEntities(vocabulary_set[i].word.de) + `</h5>
             <br>
             <div class="row">
                 <div class="col">
@@ -234,25 +270,25 @@ function createPracticeHTML(vocabulary_set, i){
         </div>
         </div>
         <div class="card-footer text-muted">
-        `+(i+1)+`/`+vocabulary_set.length+`
+        ` + (i + 1) + `/` + vocabulary_set.length + `
         </div>
         </div>`;
 
     return html;
 }
 
-function showPracticeHTML(vs,num){
-    if(document.getElementById("practice") !== null){
-        document.getElementById("practice").innerHTML = createPracticeHTML(vs,num)
+function showPracticeHTML(vs, num) {
+    if (document.getElementById("practice") !== null) {
+        document.getElementById("practice").innerHTML = createPracticeHTML(vs, num)
     }
 }
 
 // Useful functions
 
-function encodeHTMLEntities(string){
+function encodeHTMLEntities(string) {
     console.log("String:", string)
-    if(string !== undefined){
-        return string.replace(/./gm, function(s) {
+    if (string !== undefined) {
+        return string.replace(/./gm, function (s) {
             // return "&#" + s.charCodeAt(0) + ";";
             return (s.match(/[a-z0-9\s]+/i)) ? s : "&#" + s.charCodeAt(0) + ";";
         })
@@ -262,4 +298,4 @@ function encodeHTMLEntities(string){
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
