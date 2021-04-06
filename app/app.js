7148fe6a054a33d32cb1b1b86d63e8a640ab8231
Tim <4759569+TimGuenther@users.noreply.github.com>
2021-04-06 22:02:23 +0200

    Beautify and Add categories tab

--- app/app.js
+++ app/app.js
@@ -79,7 +79,7 @@ if (user_data === null) {
 }
 
 // Only on the dict page
-if (window.location.pathname == "/app/dict/"){
+if (window.location.pathname == "/app/dict/") {
     // Show count of all words in dict
     if (document.getElementById("sign_count") !== null && dict !== null) {
         document.getElementById('sign_count').innerText = dict.dict.length;
@@ -89,14 +89,14 @@ if (window.location.pathname == "/app/dict/"){
     if (document.getElementById('search') !== null) {
         document.getElementById('search').addEventListener("click", function (data) {
             word = document.getElementById("search_input").value
-            window.location.hash = '#search='+encodeURI(word);
+            window.location.hash = '#search=' + encodeURI(word);
             showSearchResult("search_result", word)
         });
     }
 
     // Enable URL Anchor #search=param to store the latest search and enable to share the query
-    if(window.location.hash.match("#search=.*?")){
-        hash_query = window.location.hash.replace("#search=","")
+    if (window.location.hash.match("#search=.*?")) {
+        hash_query = window.location.hash.replace("#search=", "")
         showSearchResult("search_result", decodeURI(hash_query))
     }
 }
@@ -115,10 +115,10 @@ if (document.getElementById("btn-stack-0") !== null) {
 //                                //
 ////////////////////////////////////
 
-function searchInDictForCategory(cat){
+function searchInDictForCategory(cat) {
     result = []
     for (var i = 0; i < dict.dict.length; i++) {
-        for(var j = 0; j < dict.dict[i].category.length; j++){
+        for (var j = 0; j < dict.dict[i].category.length; j++) {
             if (dict.dict[i].category[j].toLowerCase() === cat.toLowerCase()) {
                 result.push(dict.dict[i])
                 break;
@@ -158,9 +158,9 @@ function searchResultHTML(word, obj) {
 
     if (obj.length > 0) {
         categories = ""
-        for(var k=0;k<obj[0].category.length;k++){
+        for (var k = 0; k < obj[0].category.length; k++) {
             categories += obj[0].category[k]
-            if(k !== obj[0].category.length-1){
+            if (k !== obj[0].category.length - 1) {
                 categories += ", "
             }
         }
@@ -170,7 +170,7 @@ function searchResultHTML(word, obj) {
             <br>
             <div class="card text-center">
                 <div class="card-header">
-                    <h4 id="`+0+`">` + encodeHTMLEntities(obj[0].word.de) + `</h4>
+                    <h4 id="` + 0 + `">` + encodeHTMLEntities(obj[0].word.de) + `</h4>
                 </div>
                 <br>
                 <div class="card-img-top embed-responsive embed-responsive-4by3">
@@ -182,10 +182,10 @@ function searchResultHTML(word, obj) {
                 <div class="card-body">
                     <div class="row">
                         <div class="col">
-                        <button class="btn btn-outline-info text-left" data-toggle="collapse" data-target="#info-`+0+`" aria-expanded="false" aria-controls="info-0"><i class="fab fa-creative-commons"></i></button>
+                        <button class="btn btn-outline-info text-left" data-toggle="collapse" data-target="#info-` + 0 + `" aria-expanded="false" aria-controls="info-0"><i class="fab fa-creative-commons"></i></button>
                         </div>
                         <div class="col">
-                            <button class="btn btn-outline-success text-right" id="add-`+0+`" aria-toggle="true"><i class="fas fa-folder-plus"></i></button>
+                            <button class="btn btn-outline-success text-right" id="add-` + 0 + `" aria-toggle="true"><i class="fas fa-folder-plus"></i></button>
                         </div>
                     </div>
                     <div class="text-left collapse" id="info-0">
@@ -209,16 +209,16 @@ function searchResultHTML(word, obj) {
                 break;
             }
             categories = ""
-            for(var k=0;k<obj[i].category.length;k++){
+            for (var k = 0; k < obj[i].category.length; k++) {
                 categories += obj[i].category[k]
-                if(k !== obj[i].category.length-1){
+                if (k !== obj[i].category.length - 1) {
                     categories += ", "
                 }
             }
 
             html += `<div class="card text-center">
             <div class="card-header" data-toggle="collapse" data-target="#video-` + i + `" aria-expanded="false" aria-controls="video-` + i + `">
-                <h4 id="`+i+`">` + encodeHTMLEntities(obj[i].word.de) + `</h4>
+                <h4 id="` + i + `">` + encodeHTMLEntities(obj[i].word.de) + `</h4>
             </div>
             <div class="collapse" id="video-` + i + `">
             <div class="card-img-top embed-responsive embed-responsive-4by3">
@@ -230,10 +230,10 @@ function searchResultHTML(word, obj) {
             <div class="card-body">
                 <div class="row">
                     <div class="col">
-                        <button class="btn btn-outline-info text-left" data-toggle="collapse" data-target="#info-`+i+`" aria-expanded="false" aria-controls="info-`+i+`"><i class="fab fa-creative-commons"></i></button>
+                        <button class="btn btn-outline-info text-left" data-toggle="collapse" data-target="#info-` + i + `" aria-expanded="false" aria-controls="info-` + i + `"><i class="fab fa-creative-commons"></i></button>
                     </div>
                     <div class="col">
-                        <button class="btn btn-outline-success text-right" id="add-`+i+`" aria-toggle="true"><i class="fas fa-folder-plus"></i></button>
+                        <button class="btn btn-outline-success text-right" id="add-` + i + `" aria-toggle="true"><i class="fas fa-folder-plus"></i></button>
                     </div>
                 </div>
                     <br>
@@ -256,42 +256,42 @@ function searchResultHTML(word, obj) {
     return html;
 }
 
-function initWordAddButtons(obj){
-    console.log("initWordAddButtons",obj);
-    for(var i = 0; i<obj.length; i++){
-        toggleWordAddButton(document.getElementById("add-"+i),isWordInStack(obj[i].word.de))
+function initWordAddButtons(obj) {
+    console.log("initWordAddButtons", obj);
+    for (var i = 0; i < obj.length; i++) {
+        toggleWordAddButton(document.getElementById("add-" + i), isWordInStack(obj[i].word.de))
 
-        document.getElementById("add-"+i).addEventListener("click",function(event){
+        document.getElementById("add-" + i).addEventListener("click", function (event) {
             var element = this
             var entry = obj[this.id.replace("add-", "")]
 
             console.log(entry, this);
-            if(element !== null){
-                if(element.getAttribute("aria-toggle") === "true"){
-                    addToStack([entry],0)
-                    toggleWordAddButton(element,true)
+            if (element !== null) {
+                if (element.getAttribute("aria-toggle") === "true") {
+                    addToStack([entry], 0)
+                    toggleWordAddButton(element, true)
                 } else {
-                    for(var i=0; i<6; i++){
-                        removeFromStack([entry],0)
+                    for (var i = 0; i < 6; i++) {
+                        removeFromStack([entry], 0)
                     }
-                    toggleWordAddButton(element,false)
+                    toggleWordAddButton(element, false)
                 }
             }
         });
     }
 }
 
-function toggleWordAddButton(element, isToggeled){
-    console.log(element,isToggeled);
-    if(element!=null){
-        if(isToggeled){
+function toggleWordAddButton(element, isToggeled) {
+    console.log(element, isToggeled);
+    if (element != null) {
+        if (isToggeled) {
             element.className = "btn btn-success text-right"
             element.innerHTML = '<i class="fas fa-check"></i>'
-            element.setAttribute("aria-toggle","false")
+            element.setAttribute("aria-toggle", "false")
         } else {
             element.className = "btn btn-outline-success text-right"
             element.innerHTML = '<i class="fas fa-folder-plus"></i>'
-            element.setAttribute("aria-toggle","true")
+            element.setAttribute("aria-toggle", "true")
         }
     }
 }
@@ -304,6 +304,32 @@ function showSearchResult(id, word) {
     initWordAddButtons(obj)
 }
 
+////////////////////////////////////
+//                                //
+//      Words per Category        //
+//                                //
+////////////////////////////////////
+
+
+function searchByCategory(category) {
+    result = []
+    for (var i = 0; i < dict.dict.length; i++) {
+        for (var j = 0; j < dict.dict[i].category.length; j++) {
+            if (dict.dict[i].category[j].toLowerCase() === category.toLowerCase()) {
+                result.push(dict.dict[i])
+            }
+        }
+    }
+    return result
+}
+
+function createCategoriesHTML() {
+    html = ""
+
+    return html
+}
+
+
 
 ////////////////////////////////////
 //                                //
@@ -312,32 +338,32 @@ function showSearchResult(id, word) {
 ////////////////////////////////////
 
 
-function initPracticeStacks(){
-    for(var i = 0; i <= 5; i++){
-        stack = document.getElementById("stack-"+i);
-        stack_btn = document.getElementById("btn-stack-"+i)
-        if(stack == null || stack_btn == null){
+function initPracticeStacks() {
+    for (var i = 0; i <= 5; i++) {
+        stack = document.getElementById("stack-" + i);
+        stack_btn = document.getElementById("btn-stack-" + i)
+        if (stack == null || stack_btn == null) {
             continue;
         }
-        if(user_data.data[i] != undefined){
+        if (user_data.data[i] != undefined) {
             stack.innerText = user_data.data[i].length
-            if(user_data.data[i].length == 0){
-                stack_btn.setAttribute("disabled","")
-                stack_btn.setAttribute("aria-disabled","true")
+            if (user_data.data[i].length == 0) {
+                stack_btn.setAttribute("disabled", "")
+                stack_btn.setAttribute("aria-disabled", "true")
                 stack_btn.className = "btn btn-outline-secondary"
             }
         }
-    }  
+    }
 }
 
 /**
  * Evaluates if a word exists in one of the learning stacks
  * @param {string} word 
  */
-function isWordInStack(word){
-    for(var i=0; i<user_data.data.length; i++){
-        for(var j=0; j<user_data.data[i].length; j++){
-            if(user_data.data[i][j].word.de == word){
+function isWordInStack(word) {
+    for (var i = 0; i < user_data.data.length; i++) {
+        for (var j = 0; j < user_data.data[i].length; j++) {
+            if (user_data.data[i][j].word.de == word) {
                 return true
             }
         }
@@ -352,8 +378,8 @@ function isWordInStack(word){
  */
 function addToStack(obj, num) {
     if (num >= 0 && num < 6) {
-        for(var i = 0; i<obj.length; i++){
-            console.log("addToStack["+num+"]",obj[i]);
+        for (var i = 0; i < obj.length; i++) {
+            console.log("addToStack[" + num + "]", obj[i]);
             user_data.data[num].push(obj[i])
             saveUserData()
         }
@@ -365,12 +391,12 @@ function addToStack(obj, num) {
  * @param {Array} obj 
  * @param {*} num 
  */
-function removeFromStack(obj, num){
+function removeFromStack(obj, num) {
     if (num >= 0 && num < 6) {
-        for(var i = 0; i<obj.length; i++){
-            console.log("removeFromStack["+num+"]",obj[i]);
-            tmp = user_data.data[num].filter(function(item){
-                console.log(obj[i],item);
+        for (var i = 0; i < obj.length; i++) {
+            console.log("removeFromStack[" + num + "]", obj[i]);
+            tmp = user_data.data[num].filter(function (item) {
+                console.log(obj[i], item);
                 return obj[i].word.de !== item.word.de
             })
             user_data.data[num] = tmp
@@ -483,6 +509,6 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function saveUserData(){
+function saveUserData() {
     localStorage.setItem("user_data", JSON.stringify(user_data))
 }
