45c3db12b33dc1a0ec5038063ad1a7b1f784a0a1
Tim <4759569+TimGuenther@users.noreply.github.com>
2021-04-07 21:51:57 +0200

    Improve settings

--- app/app.js
+++ app/app.js
@@ -734,6 +734,19 @@ function resetAll(){
     window.location.href = "/app/settings/"
 }
 
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+  
+    element.click();
+  
+    document.body.removeChild(element);
+  }
+
 ////////////////////////////////////
 //                                //
 //        Useful Functions        //
